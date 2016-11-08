8c54e987fa4a6857f66a791c19c54b8abdb02709
liu15r <liu15r@dhcp-gs-6685.eduroam.cornell.edu>
2016-11-07 20:20:41 -0500

    controller

---
+++ controller.mli
@@ -0,0 +1,10 @@
+open model 
+
+
+
+(* [apply_diff d] takes in a difference and returns an updated
+ * minimodel based on the diff.*)
+val apply_diff: diff -> world
+
+(* [interpret_command c] returns a diff based on a command*)
+val interpret_command: command -> diff
