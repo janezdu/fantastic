8d9035643cade4a9e3362251be12f967a771a0e4
Paviya Tojaroon <EllesMacbook@dhcp-mcfad-12485.eduroam.cornell.edu>
2016-11-07 23:43:11 -0500

    modified cli.mli and delted client.mli

--- client/cli.mli
+++ client/cli.mli
@@ -1,4 +1,4 @@
-open model  
+open model
 open Controller
 
 (* directive is what the player types into the command line*)
@@ -6,16 +6,23 @@ type directive = string
 
 (* command is a variant type of the possible commands a player
  * could make *)
-type command = Move of string | Spell of string | Quit 
-             | Take of string | Drop of string  | Look 
-             | Inventory | Drink of string | ViewState 
-             | Help
+type command =
+  | Move of string
+  | Spell of string
+  | Quit
+  | Take of string
+  | Drop of string
+  | Look
+  | Inventory
+  | Drink of string
+  | ViewState
+  | Help
 
 (* [parse_comm d] is the command type that results from the player's
  * typed directrive. *)
 val parse_comm: directive -> command
 
-(* [view_world m] returns a string description of the world to the 
+(* [view_world m] returns a string description of the world to the
  * player based on the minimodel of the game*)
-val view_world: world -> string
+val view_world: world -> unit
 
