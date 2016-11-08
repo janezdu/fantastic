b1d0a144c78adf3b26c9942831ede36920b95e64
liu15r <liu15r@dhcp-gs-6685.eduroam.cornell.edu>
2016-11-07 20:22:47 -0500

    clienthttp

---
+++ client/clienthttp.mli
@@ -0,0 +1,12 @@
+open model  
+open Controller
+open Server
+
+(* [translate_to_json d] returns a json based on a diff *)
+val translate_to_json: diff -> json
+
+(* [translate_to_diff j] returns a diff based on a json *)
+val translate_to_diff: json -> diff
+
+(* [send_json j} sends a json to the servers. Returns unit *)
+val send_json: json -> unit
