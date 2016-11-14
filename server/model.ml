180af2851bbe1bf60433221196d8dd6f8887267f
Jane Du <zd53@cornell.edu>
2016-11-14 16:24:28 -0500

    Preliminary changes to model

--- server/model.ml
+++ server/model.ml
@@ -187,4 +187,9 @@ type world =  {
 }
 
 
+(* Takes in a diff and a world, and updates the world by applying the changes
+ * from the diff *)
+val applydiff : world -> diff -> world
 
+(* [validate w d] returns true if applying [d] to [w] is legal, false ow*)
+val validate: world -> diff -> bool
--- server/model.mli
+++ server/model.mli
@@ -18,3 +18,9 @@ type diff_item
  * and dictionary of room associated with items in the room [witems] *)
 type world
 
+(* Takes in a diff and a world, and updates the world by applying the changes
+ * from the diff *)
+val applydiff : world -> diff -> world
+
+(* [validate w d] returns true if applying [d] to [w] is legal, false ow*)
+val validate: world -> diff -> bool
