ab3b85bd08f512615bbf87eb37a645cc962fd493
liu15r <rll249@cornell.edu>
2016-11-25 01:18:51 -0500

    more stuff

--- client/clienthttp.mli
+++ client/clienthttp.mli
@@ -11,4 +11,3 @@ val send_post_request: jsonstring -> string -> int -> (string -> 'a) -> 'a
 
 (* [send_post_request j} sends a get request to the servers. Returns unit *)
 val send_get_request: jsonstring -> string  -> int -> (string -> 'a) -> 'a
->>>>>>> origin/elle
