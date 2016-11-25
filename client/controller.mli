a6115692ef72ed673d5926d683e26395257adbbe
liu15r <rll249@cornell.edu>
2016-11-25 01:16:35 -0500

    stuff

--- client/controller.mli
+++ client/controller.mli
@@ -5,7 +5,17 @@ open Clienthttp
 type command
 type diff
 type diff_json
-type comm_json
+type comm_json =
+  | JMove of string
+  | JDrink of string
+  | JSpell of string
+  | JQuit
+  | JTake of string
+  | JDrop of string
+  | JLook
+  | JInv
+  | JViewState
+  | JHelp
 
 (* [translate_to_diff j] returns diffs based on a diff json string *)
 val translate_to_diff: diff_json -> diff list
