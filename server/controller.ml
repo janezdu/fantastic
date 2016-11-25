76719d32c8c90f5cab558f6c0b52b4335f639912
Jane Du <zd53@cornell.edu>
2016-11-25 10:47:22 -0500

    Resolve type checking

--- server/controller.ml
+++ server/controller.ml
@@ -48,21 +48,26 @@ let translate_to_diff snapshot j r cid =
     let newx = json |> member "newx" |> to_int in
     let newy = json |> member "newy" |> to_int in
     [
-    Remove {loc=(curx, cury); id=cid; newitem=IVoid};
-    Add {loc=(newx, newy); id=cid; newitem=IVoid}
+      Remove {loc=(curx, cury); id=cid; newitem=IVoid};
+      Add {loc=(newx, newy); id=cid; newitem=IVoid}
     ]
   end
   else if r = "use" then begin
     try
       let item_id = json |> member "id" |> to_int in
+      let item = flatworld.items |> LibMap.find item_id in
+      let effect = match item with
+        | ISpell {effect} -> effect
+        | IPotion {effect} -> effect
+      in
       let target_id = json |> member "target" |> to_int in
       let new_inv = remove_from_list item_id player.inventory in
       let _ = remove_from_list target_id player.inventory in
       let wrapped_target = flatworld.items |> LibMap.find target_id in
-      let target = match wrapped_target with
-        | IPlayer x -> x
-        (* TODO: pattern match properly *)
-        (* | IPolice x | IAnimal x -> x *)
+      let (new_target, target_hp) = match wrapped_target with
+        | IPlayer x -> (IPlayer {x with hp = x.hp + effect}, x.hp)
+        | IPolice x -> (IPolice {x with hp = x.hp + effect}, x.hp)
+        | IAnimal x -> (IAnimal {x with hp = x.hp + effect}, x.hp)
         | _ -> failwith "not a player/ai"
       in
       let wrapped_item = flatworld.items |> LibMap.find item_id in
@@ -70,15 +75,14 @@ let translate_to_diff snapshot j r cid =
       | ISpell x ->
         begin
           let diff_target =
-            if target.hp + x.effect <= 0
+            if target_hp + x.effect <= 0
             then Remove {loc=cur_loc; id=target_id; newitem=IVoid}
             else Change {loc=cur_loc; id=target_id;
-                newitem = IPlayer {target with hp = target.hp + x.effect}}
+                         newitem=new_target}
           in
           [
-            (* TODO: can't always wrap in IPlayer *)
-          Change {loc=cur_loc; id=cid; newitem= IPlayer {player with inventory=new_inv}};
-          diff_target
+            Change {loc=cur_loc; id=cid; newitem=IPlayer {player with inventory=new_inv}};
+            diff_target
           ]
         end
       | IPotion x ->
@@ -87,55 +91,62 @@ let translate_to_diff snapshot j r cid =
             if player.hp + x.effect <= 0
             then Remove {loc=cur_loc; id=cid; newitem=IVoid}
             else Change {loc=cur_loc; id=cid;
-                newitem= IPlayer {player with hp = player.hp + x.effect}}
+                         newitem=IPlayer {player with hp = player.hp + x.effect}}
           in
           [
-          Change {loc=cur_loc; id=cid; newitem= IPlayer {player with inventory=new_inv}};
-          diff_player
+            Change {loc=cur_loc; id=cid; newitem=IPlayer {player with inventory=new_inv}};
+            diff_player
           ]
         end
-      | _ -> failwith "not a spell/potion"
-    with _ -> raise IllegalUse
-  end
-  else if r = "take" then begin
-    try
-      let item_id = json |> member "id" |> to_int in
-      let _ = remove_from_list item_id cur_room.items in
-      [
+    | _ -> failwith "not a spell/potion"
+with _ -> raise IllegalUse
+end
+else if r = "take" then begin
+  try
+    let item_id = json |> member "id" |> to_int in
+    let _ = remove_from_list item_id cur_room.items in
+    let wrapped_item = flatworld.items |> LibMap.find item_id in
+    let valid_item = match wrapped_item with
+      | ISpell _ | IPotion _ -> true | _ -> failwith "not a spell/potion"
+    in
+    [
       Remove {loc=cur_loc; id=item_id; newitem=IVoid};
       Change {loc=cur_loc; id=cid;
-        newitem = IPlayer {player with inventory=item_id::player.inventory}}
-      ]
-    with _ -> raise IllegalTake
-  end
-  else if r = "drop" then begin
-    try
-      let item_id = json |> member "id" |> to_int in
-      let _ = remove_from_list item_id player.inventory in
-      let new_inv = remove_from_list item_id player.inventory in
-      [
-        Change {loc = cur_loc; id = cid;
-                newitem = IPlayer{player with inventory = new_inv}};
-        Add {loc=cur_loc; id=item_id; newitem=IVoid}
-      ]
-    with
-    | _ -> raise IllegalDrop
-  end
-  else
-    []
+              newitem=IPlayer {player with inventory=item_id::player.inventory}}
+    ]
+  with _ -> raise IllegalTake
+end
+else if r = "drop" then begin
+  try
+    let item_id = json |> member "id" |> to_int in
+    let wrapped_item = flatworld.items |> LibMap.find item_id in
+    let valid_item = match wrapped_item with
+      | ISpell _ | IPotion _ -> true |_ -> failwith "not a spell/potion"
+    in
+    let _ = remove_from_list item_id player.inventory in
+    let new_inv = remove_from_list item_id player.inventory in
+    [
+      Change {loc=cur_loc; id=cid; newitem=IPlayer {player with inventory=new_inv}};
+      Add {loc=cur_loc; id=item_id; newitem=IVoid}
+    ]
+  with
+  | _ -> raise IllegalDrop
+end
+else
+  []
 
 (* side effects to flatworld and client_diffs *)
 let rec step cid diffs = match diffs with
-| [] -> ()
-| (loc, items)::t -> begin
+  | [] -> ()
+  | (loc, items)::t -> begin
 
-    ()
-  end
+      ()
+    end
 
 let rec remove x l = match l with
   | [] -> failwith "no such element"
   | h::t -> if h = x then t
-            else h::(remove x t)
+    else h::(remove x t)
 
 (* returns the most up-to-date timestamp based on the server state *)
 let curtime state  = failwith "unimplemented"
@@ -143,13 +154,13 @@ let curtime state  = failwith "unimplemented"
 (* returns the diff for a client when it asks for an update *)
 let getClientUpdate cid =
   failwith "unimplemented"
-  (* try
-    let diff_ref = List.nth state.client_diffs cid in
-    let diff = !diff_ref in
-    diff_ref := [];
-    diff
-  with _ -> failwith "illegal client"
- *)
+(* try
+   let diff_ref = List.nth state.client_diffs cid in
+   let diff = !diff_ref in
+   diff_ref := [];
+   diff
+   with _ -> failwith "illegal client"
+*)
 
 (* [translate_to_json d] returns a json based on diffs *)
 let translate_to_json difflist =
