09f03acd353f0877e4417d871a08af16dd3392a5
Jane Du <zd53@cornell.edu>
2016-11-26 15:40:49 -0500

    Clean up server

--- server/serverhttp.ml
+++ server/serverhttp.ml
@@ -14,110 +14,82 @@ exception BadRequest of string
 (* [start pw] will start a game server; join game with password [pw] *)
 let start s = failwith "unimplemented"
 
-(* [translate_to_newuser j] will attempt to add the new user with the password
- * they have included in [j], returning a sessionid if valid. *)
-let translate_to_newuser j = failwith "unimplemented"
-
-(* [send_response j status] sends an http response to the clients *)
-let send_response j s = failwith "unimplemented"
-
-(* [send_status] sense a response without a body*)
-let send_status s = failwith "unimplemented"
-
 type mode = Login of string | Query of int | Badmode
+
+let strip path = String.sub path 1 (String.length path - 1)
 
 (* let loginsallowed = ref true *)
 
 (* A method that handles legal queries once gameplay starts. Does not handle
  * login *)
-let handleQuery req body cid =
-  match Uri.path (Request.uri req) with
-  | "/move" -> begin
-        body |> Cohttp_lwt_body.to_string >|= (fun cmdbody ->
-            ( print_endline cmdbody;
-              pushClientUpdate cid cmdbody "move"))
-        >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-    end
-  | "/use" -> begin
+let handleQuery req body cid : string Lwt.t=
+  let path = Uri.path (Request.uri req) in
+  match path with
+  | "/move" | "/use" | "/take" | "/drop" -> begin
       body |> Cohttp_lwt_body.to_string >|= (fun cmdbody ->
           ( print_endline cmdbody;
             try
-              pushClientUpdate cid cmdbody "use"
+              pushClientUpdate cid cmdbody (strip path)
             with
             | WorldFailure msg -> begin
                 print_endline "got to error";
                 raise (WorldFailure msg)
-            end))
-      >>= (fun body -> Server.respond_string ~status:`OK ~body ())
+              end
+            | _ -> failwith "Legal uri but broken non-worldfailure"))
     end
-  | "/take" -> begin
-      body |> Cohttp_lwt_body.to_string >|= (fun cmdbody ->
-          ( print_endline cmdbody;
-            pushClientUpdate cid cmdbody "take"))
-      >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-    end
-  | "/drop" -> begin
-      body |> Cohttp_lwt_body.to_string >|= (fun cmdbody ->
-          ( print_endline cmdbody;
-            pushClientUpdate cid cmdbody "drop"))
-      >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-    end
-  | "/update" -> begin
-      body |> Cohttp_lwt_body.to_string >|= (fun cmdbody ->
-          ( print_endline ("[UPDATE]: "^(string_of_int cid));
-            getClientUpdate cid))
-      >>= (fun body -> Server.respond_string ~status:`OK ~body ())
-    end
-  | _ -> begin
-    Server.respond_string ~status:`Bad_request ~body: "u dun guffed off" ()
+
+  | "/update" -> body |> Cohttp_lwt_body.to_string >|= (fun cmdbody ->
+      ( print_endline ("[UPDATE]: "^(string_of_int cid));
+        getClientUpdate cid))
+  | _ -> begin return ("u dun guffed off")
+    (* Server.respond_string ~status:`Bad_request ~body: "u dun guffed off" () *)
     end
 
 (* A method that deals with user registration only. *)
 let handleLogin req body name =
   let cid = registerUser name in
   print_endline (name ^ "'s clieentid is "^(string_of_int cid));
-  Server.respond_string ~status:`OK ~body: (string_of_int cid) ()
-
+  return (string_of_int cid)
 
 (* a server is a function that gets data, compute and respond *)
 let server =
   let callback _conn req body =
-    try
-      print_endline ("\n\n===================================================="^
-                     "\nstarted callback");
-      print_endline (req |> Request.uri |> Uri.to_string);
-      (* print_endline (Uri.path (Request.uri req)); *)
-      (* let uri = req |> Request.uri |> Uri.to_string in *)
-      (* let meth = req |> Request.meth |> Code.string_of_method in *)
-      (* let headers = req |> Request.headers |> Header.to_string in *)
-      let queryparams = req |> Request.uri |> Uri.query in
+    print_endline ("\n\n===================================================="^
+                   "\nstarted callback");
+    print_endline (req |> Request.uri |> Uri.to_string);
+    (* print_endline (Uri.path (Request.uri req)); *)
+    (* let uri = req |> Request.uri |> Uri.to_string in *)
+    (* let meth = req |> Request.meth |> Code.string_of_method in *)
+    (* let headers = req |> Request.headers |> Header.to_string in *)
+    let queryparams = req |> Request.uri |> Uri.query in
 
-      let reqmode =
-        if List.mem_assoc "client_id" queryparams then
-          let cid = List.assoc "client_id" queryparams
-                    |> List.hd |> int_of_string in
-          if (not (check_clientid cid)) then raise (BadRequest "Invalid user")
-          else Query (cid)
-        else if List.mem_assoc "username" queryparams then
-          begin
-            let name = List.assoc "username" queryparams |> List.hd in
-            (* print_endline ("New player: " ^ name); *)
-            Login (name)
-          end
-        else Badmode
-      in
+    let reqmode =
+      if List.mem_assoc "client_id" queryparams then
+        let cid = List.assoc "client_id" queryparams
+                  |> List.hd |> int_of_string in
+        if (not (check_clientid cid)) then raise (BadRequest "Invalid user")
+        else Query (cid)
+      else if List.mem_assoc "username" queryparams then
+        begin
+          let name = List.assoc "username" queryparams |> List.hd in
+          Login (name)
+        end
+      else Badmode
+    in
 
-      match reqmode with
+    let body = match reqmode with
       | Query (cid) -> begin
           print_endline ("hanlding player "^(string_of_int cid));
-          handleQuery req body cid
+          try handleQuery req body cid with | x -> raise x
         end
       | Login (name) -> begin
           print_endline ("handling login "^name);
-          handleLogin req body name
+          try handleLogin req body name with | x -> raise x
       end
       | Badmode -> raise (BadRequest ("Badly formed uri, missing query"))
+    in
 
+    try body >>=(fun body -> Server.respond_string ~status:`OK ~body ())
     with
     | WorldFailure msg -> begin
         print_endline msg;
