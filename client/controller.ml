a2d26edfc3a3355287d2218980677bd008da8910
Rebecca Liangyan Liu <rll249@cornell.edu>
2016-11-28 21:10:04 -0500

    target lowercase

--- client/controller.ml
+++ client/controller.ml
@@ -340,7 +340,7 @@ let interp_spell s  t (w:world): comm_json =
    match (find_item s w) with
    | Some s ->
      (match (find_item t w) with
-     | Some t -> JSpell ("{\"id\":" ^ (string_of_int s) ^", \"Target\":"^(string_of_int t)^"}")
+     | Some t -> JSpell ("{\"id\":" ^ (string_of_int s) ^", \"target\":"^(string_of_int t)^"}")
      | None -> raise NotAnItem)
    | None -> raise NotAnItem
 
@@ -365,7 +365,7 @@ let interp_drop d (w:world): comm_json =
 (* find room player is in, find item they want to drop in inventory*)
 let interp_drink d current_player (w:world): comm_json =
    match (find_item d w) with
-   | Some d -> JDrink ("{\"id\":" ^ (string_of_int d) ^ ", \"Target\":"^(string_of_int current_player )^"}")
+   | Some d -> JDrink ("{\"id\":" ^ (string_of_int d) ^ ", \"target\":"^(string_of_int current_player )^"}")
    | None -> raise NotAnItem
 
 (* [interpret_command c] returns a command_json list based on a command*)
